ef02faa10a9132ff100db4795f26bf8b5dbe603b
ichiroc <nobitahouse@gmail.com>
2025-01-26 23:25:43 +0900

    debug

--- install.sh
+++ install.sh
@@ -7,6 +7,9 @@ EXTENSION_ID=chatgpt2scratch
 COLLABORATOR=ichiroc
 EXTENSION_DESCRIPTION="ChatGPT for Scratch"
 
+sed -i -e "s/process.env.AZURE_API_KEY/\"${AZURE_API_KEY}\"/g" ${EXTENSION_ID}/scratch-vm/src/extensions/scratch3_${EXTENSION_ID}/index.js
+sed -i -e "s/process.env.DOJO_KEY/\"${DOJO_KEY}\"/g" ${EXTENSION_ID}/scratch-vm/src/extensions/scratch3_${EXTENSION_ID}/index.js
+
 mkdir -p node_modules/scratch-vm/src/extensions/scratch3_${EXTENSION_ID}
 cp ${EXTENSION_ID}/scratch-vm/src/extensions/scratch3_${EXTENSION_ID}/index.js node_modules/scratch-vm/src/extensions/scratch3_${EXTENSION_ID}/
 mv node_modules/scratch-vm/src/extension-support/extension-manager.js node_modules/scratch-vm/src/extension-support/extension-manager.js_orig
@@ -36,5 +39,3 @@ DESCRIPTION="\
         bluetoothRequired: false${LF}\
     },"
 sed -e "s|^export default \[$|import ${EXTENSION_ID}IconURL from './${EXTENSION_ID}/${EXTENSION_ID}.png';${LF}import ${EXTENSION_ID}InsetIconURL from './${EXTENSION_ID}/${EXTENSION_ID}-small.png';${LF}${LF}export default [${LF}${DESCRIPTION}|g" src/lib/libraries/extensions/index.jsx_orig > src/lib/libraries/extensions/index.jsx
-sed -e "s/process.env.AZURE_API_KEY/"${AZURE_API_KEY}"/g" node_modules/scratch-vm/src/extensions/scratch3_${EXTENSION_ID}/index.js > node_modules/scratch-vm/src/extensions/scratch3_${EXTENSION_ID}/index.js
-sed -e "s/process.env.DOJO_KEY/"${DOJO_KEY}"/g" node_modules/scratch-vm/src/extensions/scratch3_${EXTENSION_ID}/index.js > node_modules/scratch-vm/src/extensions/scratch3_${EXTENSION_ID}/index.js
